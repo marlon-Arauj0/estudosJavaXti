1f0e9797feeb44601a3d4a31fc09ecee02abf8de
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-04-23 21:42:40 -0300

    Primeiro commit em java

---
+++ Variavel.java
@@ -0,0 +1,16 @@
+/**
+ *Conceitos de variavel
+ *@author Marlon Silva Araujo
+ */ 
+public class Variavel{
+	public static void main(String[] args){
+	
+	  String nome = "Marlon";
+	  int idade = 20;
+	  boolean casado = false;
+	  
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(casado);
+	}
+}
