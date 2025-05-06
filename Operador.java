6a1fa1acdd186513172d6e04cc2beec0f2e1bc1c
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-05-06 19:43:49 -0300

    Operadores em geral

---
+++ Operador.java
@@ -0,0 +1,10 @@
+public class Operador {
+	public static void main (String[] args){
+		//2 + 3 binário
+		//-2 unário
+		//true ? "sim" : "não" operador ternário
+		
+		int x = 9 + 4;
+		System.out.println(x);
+	}
+}
