575baf3cd7a3c6193b587b75915bff7fb54ad15f
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-05-06 19:53:08 -0300

    Operadores em geral pt.02

--- Operador.java
+++ Operador.java
@@ -1,10 +1,20 @@
 public class Operador {
+
 	public static void main (String[] args){
 		//2 + 3 binário
 		//-2 unário
 		//true ? "sim" : "não" operador ternário
 		
-		int x = 9 + 4;
-		System.out.println(x);
+		int a = 9 + 4;
+		System.out.println(a);
+		
+		String b = "9" + "4";
+		System.out.println(b);
+		
+		double c = 7 - 4 + 3 * 2;
+		System.out.println(c);
+		
+		double d = (7 - 4 + 3) * 2;
+		System.out.println(d); 
 	}
 }
