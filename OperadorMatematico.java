170801a70a16b1664dece461f33338351ab372d8
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-05-07 06:03:18 -0300

    operadores matematicos

---
+++ OperadorMatematico.java
@@ -0,0 +1,23 @@
+public class OperadorMatematico {
+
+	public static void main (String[] args) {
+	  double e = 7 + 3;
+	  System.out.println(e);
+	  
+	  double f = 7 - 3;
+	  System.out.println(f);
+	  
+	  double g = 7 / 3;
+	  System.out.println(g);
+	  
+	  double h = 7 % 3;
+	  System.out.println(h);
+	  
+	  double i = 7 * 3;
+	  System.out.println(i);
+	  
+	  String j = "Oi " + "Programador Java";
+	  System.out.println(j);
+	
+	}
+}
