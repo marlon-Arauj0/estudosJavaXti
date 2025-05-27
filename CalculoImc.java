016697ed33b66f9c650cdc77053351458fd7860c
marlon-Arauj0 <marlonaraujo1303@gmail.com>
2025-05-26 21:59:00 -0300

    calculadora imc

---
+++ CalculoImc.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CalculoImc {
+
+  public static void main (String[] args) {
+  	
+  	Scanner s = new Scanner(System.in);
+  	
+  	System.out.println("Informe seu peso");
+  	double peso = s.nextDouble();
+  	
+  	System.out.println("informe sua altura em cm");
+  	double altura = s.nextDouble();
+  	
+  	double imc = peso / (altura * altura);
+  	System.out.println(imc);
+  	
+  	if (imc < 20){
+  		System.out.println("Abaixo do peso");  	
+  	} else if (imc <= 25){
+  		System.out.println("Peso ideal");
+  	} else if (imc <= 30){
+  		System.out.println("Sobrepeso");
+  	} else if (imc <= 35){
+  		System.out.println("Obesidade Moderada");
+  	} else if (imc <= 40){
+  		System.out.println("Obesidade Severa");
+  	} else if (imc < 50){
+  		System.out.println("Obesidade Mórbida");
+  	} else {
+  		System.out.println("Super-Obesidade");
+  	}
+  }
+} 
